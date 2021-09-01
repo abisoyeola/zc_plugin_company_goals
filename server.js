5ff85cbb92290668fca7aac2995ca2321107581d
abisoyeola <olaabisoye@gmail.com>
2021-09-01 23:12:58 +0100

    Port
    Another number could be use, not neccessarily 8080

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import loadClient from './middlewares/load_client.js';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; // If the port is busy , you can change to any other number , prefferably 4 in lenght.
 
 const app = express();
 
 loadClient(app);
 
 app.listen(PORT, () => {
-  console.log(`Server Started on Port: ${PORT}\nMode: ${process.env.NODE_ENV}\nURI: http://127.0.0.1:${PORT}`);
+    console.log(`Server Started on Port: ${PORT}\nMode: ${process.env.NODE_ENV}\nURI: http://127.0.0.1:${PORT}`);
 });
